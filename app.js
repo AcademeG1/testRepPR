27345494c5730852c483443145262ffde9ce3438
Misha <36785100+AcademeG1@users.noreply.github.com>
2022-10-31 13:36:48 +0300

    fix: create section and add element

---
+++ app.js
@@ -0,0 +1,9 @@
+<section class="main__add-item add-item">
+      <h3 class="add-item__title title">Add Item</h3>
+      <div class="add-item__container item-container">
+        <label class="item-container__label" for="item-container__label_js">
+          <input class="item-container__input item-container__input_active input-text" type="text" id="item-container__label_js">
+        </label>
+        <button class="item-container__button button">Add</button>
+      </div>
+</section>
